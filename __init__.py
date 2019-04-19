b6f4198433b9f0af2aca3052d4c4a06307b0a4fe
Kris Gesling <krisgesling@gmail.com>
2019-04-19 14:17:51 +0930

    hardcode pi directory

--- __init__.py
+++ __init__.py
@@ -3,11 +3,11 @@ from mycroft.messagebus.message import Message
 from os.path import join, exists
 from time import sleep
 
-
 class SkillTesting(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        self.data_file = '/'.join([self.file_system.path, 'phrases.txt'])
+        # self.data_file = '/'.join([self.file_system.path, 'phrases.txt'])
+        self.data_file = '/home/pi/.mycroft/skills/SkillTesting/phrases.txt'
 
     @intent_file_handler('testing.skill.intent')
     def handle_testing_skill(self, message):
@@ -16,6 +16,7 @@ class SkillTesting(MycroftSkill):
         f = open(self.data_file, 'r')
         phrases = f.readlines()
         # Loop over each emitting message
+
         for phrase in phrases:
             self.speak_dialog('testing.phrase', {'phrase': phrase})
             self.bus.emit(Message("recognizer_loop:utterance",
